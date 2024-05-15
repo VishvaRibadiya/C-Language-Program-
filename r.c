de09854c20241c5f2d9f50298cc8b276d6cd6b41
VishvaRibadiya <166017828+VishvaRibadiya@users.noreply.github.com>
2024-05-15 09:40:39 +0530

    demo

---
+++ r.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a,b,sum;
+
+    printf("\n Enter the number of A:");
+    scanf("%d",&a);
+
+    printf("\n Enter the value of B:");
+    scanf("%d",&b);
+
+    sum=a+b;
+    printf("Your ans is: %d",sum);
+
+    return 0;
+}
